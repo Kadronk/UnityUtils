acbdcb47426ca8383e8df10b488b83c2c325c59f
Kadronk <kadronk@kadronk.kad>
2023-11-06 22:49:53 +0100

    Non-unfoldable field labels now use the whole property width

--- PolymorphicFields/Editor/PolymorphableDrawer.cs
+++ PolymorphicFields/Editor/PolymorphableDrawer.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -19,10 +17,10 @@ namespace Kadronk.PolymorphicFields.Editor
             if (property.managedReferenceValue != null) {
                 float buttonWidth = GUI.skin.button.CalcSize(_contChangeType).x + 8.0f;
                 Rect rectChangeType = position;
-                rectChangeType.xMin += position.width - buttonWidth;
+                rectChangeType.xMin = position.width - buttonWidth;
                 rectChangeType.height = EditorGUIUtility.singleLineHeight;
                 if (EditorGUI.DropdownButton(rectChangeType, _contChangeType, FocusType.Keyboard)) {
-                    SelectTypeMenu();
+                    SelectTypeMenu(property);
                 }
 
                 if (attribute != null) {
@@ -40,7 +38,14 @@ namespace Kadronk.PolymorphicFields.Editor
                     }
                 }
 
-                EditorGUI.PropertyField(position, property, label, true);
+                if (property.hasVisibleChildren)
+                    EditorGUI.PropertyField(position, property, label, true);
+                else {
+                    float oldWidth = EditorGUIUtility.labelWidth;
+                    EditorGUIUtility.labelWidth = position.width;
+                    EditorGUI.PropertyField(position, property, label, true);
+                    EditorGUIUtility.labelWidth = oldWidth;
+                }
                 return;
             }
 
@@ -48,48 +53,48 @@ namespace Kadronk.PolymorphicFields.Editor
             Rect rectSelectClass = position;
             rectSelectClass.xMin += EditorGUIUtility.labelWidth;
             if (EditorGUI.DropdownButton(rectSelectClass, _contSelectType, FocusType.Keyboard)) {
-                SelectTypeMenu();
-            }
-
-            void SelectTypeMenu() {
-                Type parentType = fieldInfo.FieldType;
-                if (parentType.IsArray)
-                    parentType = parentType.GetElementType();
-                else if (parentType.IsGenericType && parentType.GetGenericTypeDefinition() == typeof(List<>))
-                    parentType = parentType.GetGenericArguments()[0];
-                List<Type> types = new List<Type>(parentType.GetSubtypes());
-                if (attribute != null && ((PolymorphableAttribute)attribute).IncludeParent) {
-                    ++types.Capacity;
-                    types.Insert(0, parentType);
-                }
-                string[] typesStr = new string[types.Count];
-                for (int i = 0; i < types.Count; i++) {
-                    typesStr[i] = types[i].GetDisplayName();
-                }
-                
-                GenericMenu menu = new GenericMenu();
-                for (int i = 0; i < types.Count; i++) {
-                    menu.AddItem(new GUIContent(typesStr[i]), property.managedReferenceValue != null && property.managedReferenceValue.GetType() == types[i], CreateInstance, types[i]);
-                }
-                menu.AddSeparator(string.Empty);
-                menu.AddItem(_contSetToNull, false, RemoveInstance);
-                menu.ShowAsContext();
-                
-                void CreateInstance(object type) {
-                    property.managedReferenceValue = Activator.CreateInstance((Type)type);
-                    property.serializedObject.ApplyModifiedProperties();
-                }
-
-                void RemoveInstance() {
-                    property.managedReferenceValue = null;
-                    property.serializedObject.ApplyModifiedProperties();
-                }
+                SelectTypeMenu(property);
             }
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
             return EditorGUI.GetPropertyHeight(property);
+        }
+        
+        void SelectTypeMenu(SerializedProperty property) {
+            Type parentType = fieldInfo.FieldType;
+            if (parentType.IsArray)
+                parentType = parentType.GetElementType();
+            else if (parentType.IsGenericType && parentType.GetGenericTypeDefinition() == typeof(List<>))
+                parentType = parentType.GetGenericArguments()[0];
+            List<Type> types = new List<Type>(parentType.GetSubtypes());
+            if (attribute != null && ((PolymorphableAttribute)attribute).IncludeParent) {
+                ++types.Capacity;
+                types.Insert(0, parentType);
+            }
+            string[] typesStr = new string[types.Count];
+            for (int i = 0; i < types.Count; i++) {
+                typesStr[i] = types[i].GetDisplayName();
+            }
+                
+            GenericMenu menu = new GenericMenu();
+            for (int i = 0; i < types.Count; i++) {
+                menu.AddItem(new GUIContent(typesStr[i]), property.managedReferenceValue != null && property.managedReferenceValue.GetType() == types[i], CreateInstance, types[i]);
+            }
+            menu.AddSeparator(string.Empty);
+            menu.AddItem(_contSetToNull, false, RemoveInstance);
+            menu.ShowAsContext();
+                
+            void CreateInstance(object type) {
+                property.managedReferenceValue = Activator.CreateInstance((Type)type);
+                property.serializedObject.ApplyModifiedProperties();
+            }
+
+            void RemoveInstance() {
+                property.managedReferenceValue = null;
+                property.serializedObject.ApplyModifiedProperties();
+            }
         }
     }
 }
